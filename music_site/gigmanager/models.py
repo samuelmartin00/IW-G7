4af7d9c29baa119b5f5107482110395d1f1de0b8
DaddyShortMember <Marioepf@Opendeusto.es>
2023-11-07 21:50:54 +0100

    App instalada e inicializada
    Ahora es cuestion de mejorar las relaciones entre los modelos, ver que mas se le puede meter, y hacer las vistas.

--- music_site/gigmanager/models.py
+++ music_site/gigmanager/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Grupo(models.Model):
-    nombre = models.CharField(max_length=40)
-    integrante = models.ForeignKey(Musico, on_delete=models.CASCADE)   
-
+   
 
 class Musico(models.Model):   
     # Campo para la relación   
     nombre = models.CharField(max_length=40)   
     fecha_nacimiento = models.DateField()   # Es posible indicar un valor por defecto mediante 'default'
+
+class Grupo(models.Model):
+    nombre = models.CharField(max_length=40)
+    integrante = models.ForeignKey(Musico, on_delete=models.CASCADE)
 
 class Sitio(models.Model):
     nombre = models.CharField(max_length=40)
