b4ef911567cc3812580e08d454cab715c06818c7
DaddyShortMember <Marioepf@Opendeusto.es>
2023-11-08 15:28:24 +0100

    Cambios a codigo
    Ahora si ejecuta de manera semi-correcta. Seguire luego, que tengo ensayo

--- music_site/gigmanager/views.py
+++ music_site/gigmanager/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse("HolaMundo")
+
+def grupo(request):
+    return HttpResponse("")
+
+def sala(request):
+    return HttpResponse("")
+
+def musico(request):
+    return HttpResponse("")
