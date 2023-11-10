f1276bd633fce1acfd61072aad1614bc36203240
DaddyShortMember <Marioepf@Opendeusto.es>
2023-11-10 17:16:24 +0100

    Modelos aun mas mejorados
    Hola Mundo.

--- music_site/gigmanager/models.py
+++ music_site/gigmanager/models.py
@@ -1,22 +1,18 @@
 from django.db import models
 
 # Create your models here.
-   
-
+#Clase Genero, posibilidad
+class Grupo(models.Model):
+    nombre = models.CharField(max_length=40)
+    descripcion = models.CharField(max_length=100, blank = True, null=True)
+    
 class Musico(models.Model):   
     # Campo para la relación   
     nombre = models.CharField(max_length=40)   
     fecha_nacimiento = models.DateField()   # Es posible indicar un valor por defecto mediante 'default'
-
-class Grupo(models.Model):
-    nombre = models.CharField(max_length=40)
-    lider = models.ForeignKey(Musico, on_delete=models.CASCADE)
-    descripcion = models.CharField(max_length=100)
-
-class Integrante(models.Model):
-    musico = models.ForeignKey(Musico, on_delete=models.CASCADE)
-    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE, blank = True, null=True)   
 
 class Sitio(models.Model):
     nombre = models.CharField(max_length=40)
     aforo = models.IntegerField(blank = True, null=True)
+    direccion = models.CharField(max_length=100, blank = True, null=True)
