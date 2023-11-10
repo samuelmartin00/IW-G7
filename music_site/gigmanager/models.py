f1e410d75e42f47e29afc97beda040814ac86009
ibaipuertas <ibaipuertasq@opendeusto.es>
2023-11-10 16:49:03 +0100

    Update models.py

--- music_site/gigmanager/models.py
+++ music_site/gigmanager/models.py
@@ -11,6 +11,7 @@ class Musico(models.Model):
 class Grupo(models.Model):
     nombre = models.CharField(max_length=40)
     lider = models.ForeignKey(Musico, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=100)
 
 class Integrante(models.Model):
     musico = models.ForeignKey(Musico, on_delete=models.CASCADE)
