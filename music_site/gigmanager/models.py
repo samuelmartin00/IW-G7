dc1d941c156de98bc9595a86354aae6ba984375f
DaddyShortMember <Marioepf@Opendeusto.es>
2023-11-10 17:34:24 +0100

    MODELOS FINALIZADOS.

--- music_site/gigmanager/models.py
+++ music_site/gigmanager/models.py
@@ -2,15 +2,16 @@ from django.db import models
 
 # Create your models here.
 #Clase Genero, posibilidad
-class Grupo(models.Model):
-    nombre = models.CharField(max_length=40)
-    descripcion = models.CharField(max_length=100, blank = True, null=True)
     
 class Musico(models.Model):   
     # Campo para la relación   
     nombre = models.CharField(max_length=40)   
-    fecha_nacimiento = models.DateField()   # Es posible indicar un valor por defecto mediante 'default'
-    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE, blank = True, null=True)   
+    fecha_nacimiento = models.DateField()   # Es posible indicar un valor por defecto mediante 'default'  
+    
+class Grupo(models.Model):
+    nombre = models.CharField(max_length=40)
+    descripcion = models.CharField(max_length=100, blank = True, null=True)
+    integrante = models.ForeignKey(Musico, on_delete=models.CASCADE, blank = True, null=True)
 
 class Sitio(models.Model):
     nombre = models.CharField(max_length=40)
