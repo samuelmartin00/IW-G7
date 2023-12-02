740c824d533c0499fb125e449978e06c2719e99b
samuelmartin00 <samuel.martin@opendeusto.es>
2023-12-02 15:19:03 +0100

    Audio file and Lyrics
    Mejora de modelo de datos y relaciones.
    Audio+Letra+Descripción

--- MusicProject/MusicArchive/models.py
+++ MusicProject/MusicArchive/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django_cleanup import cleanup
 
 class Genre(models.Model):
     name = models.CharField(max_length=100)
@@ -7,23 +8,31 @@ class Genre(models.Model):
 class Artist(models.Model):
     name = models.CharField(max_length=200)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='artist_images/', null=True, blank=True)  # Define the ImageField
+    image = models.ImageField(upload_to='artist_images/', null=True, blank=True)
+    description = models.TextField(blank=True)  # Added description field
 
     def __str__(self):
         return self.name
-    # Agrega otros campos relevantes para el artista
 
 class Album(models.Model):
     title = models.CharField(max_length=200)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='albums')
     year = models.IntegerField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-    # Agrega otros campos relevantes para el álbum
+    description = models.TextField(blank=True)
 
 class Song(models.Model):
     title = models.CharField(max_length=255)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True, blank=True)  # nullable field
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True, blank=True)
     duration = models.DurationField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-    # Agrega otros campos relevantes para la canción
+    audio_file = models.FileField(upload_to='song_audio/', null=True, blank=True)
+    description = models.TextField(blank=True)
+    lyrics = models.TextField(blank=True)
+
+    @property
+    def audio_url(self):
+        if self.audio_file and hasattr(self.audio_file, 'url'):
+            return self.audio_file.url
+        return ''
